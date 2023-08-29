d96d590b8c8bf2e9220a2d07818fcb902734341a
Fabricio Simbaña <70413460+SrBebo@users.noreply.github.com>
2023-08-28 21:34:58 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,93 +3,110 @@ import 'package:flutter/material.dart';
 import 'package:pokedex_mobile/firebase_options.dart';
 import 'package:pokedex_mobile/providers/category_provider.dart';
 import 'package:pokedex_mobile/providers/pokemon_provider.dart';
+import 'package:pokedex_mobile/providers/login_provider.dart';
+import 'package:provider/provider.dart';
+import 'package:pokedex_mobile/screens/category_screen.dart';
 import 'package:pokedex_mobile/screens/pokemon_details.dart';
 import 'package:pokedex_mobile/screens/pokemon_favorite_list.dart';
 import 'package:pokedex_mobile/screens/pokemon_screen.dart';
-import 'package:provider/provider.dart';
-import 'screens/category_screen.dart';
-import 'package:pokedex_mobile/widgets/login.dart';
-import 'package:pokedex_mobile/providers/login_provider.dart';
+import 'package:pokedex_mobile/screens/login.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-        providers: [
-          ChangeNotifierProvider(create: (context) => CategoryProvider()),
-          ChangeNotifierProvider(create: (context) => PokemonProvider()),
-          ChangeNotifierProvider(create: (context) => LoginProvider()),
-        ],
-        child: MaterialApp(
-            title: 'Pokedex',
-            initialRoute: MainWidget.routeName,
-            routes: {
-              MainWidget.routeName: (context) => const MainWidget(),
-              PokemonDetailsScreen.routeName: (context) =>
-                  const PokemonDetailsScreen(),
-              LoginScreen.routeName: (context) => LoginScreen(),
-            }));
+      providers: [
+        ChangeNotifierProvider(create: (context) => CategoryProvider()),
+        ChangeNotifierProvider(create: (context) => PokemonProvider()),
+        ChangeNotifierProvider(create: (context) => LoginProvider()),
+      ],
+      child: MaterialApp(
+        title: 'Pokedex',
+        home: AppRootWidget(),
+        routes: {
+          // Usar los nombres de rutas en formato de cadena
+          '/category': (context) => CategoryScreen(),
+          '/pokemon': (context) => PokemonScreenWidget(),
+          '/favorites': (context) => PokemonFavoriteListScreen(),
+          LoginScreen.routeName: (context) => LoginScreen(),
+        },
+      ),
+    );
+  }
+}
+
+class AppRootWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final loginProvider = Provider.of<LoginProvider>(context);
+
+    if (loginProvider.isLoggedIn) {
+      return MainWidget();
+    } else {
+      return LoginScreen();
+    }
   }
 }
 
 class MainWidget extends StatefulWidget {
-  static const routeName = '/';
-
-  const MainWidget({super.key});
-
   @override
-  State<MainWidget> createState() => _MainWidgetState();
+  _MainWidgetState createState() => _MainWidgetState();
 }
 
 class _MainWidgetState extends State<MainWidget> {
-  int _selectIndex = 0;
+  int _selectedIndex = 0;
 
-  final Map<int, Widget> _mainWidgets = {
-    0: const CategoryScreen(),
-    1: const PokemonScreenWidget(),
-    2: const PokemonFavoriteListScreen(),
-  };
+  final List<Widget> _mainWidgets = [
+    CategoryScreen(),
+    PokemonScreenWidget(),
+    PokemonFavoriteListScreen(),
+  ];
 
-  void _onTapItem(int index) {
-    if (index == 3) {
-      // Navegar a la pantalla de inicio de sesión
-      Navigator.of(context).pushNamed(LoginScreen.routeName);
-    } else {
-      setState(() {
-        _selectIndex = index;
-      });
-    }
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+    final loginProvider = Provider.of<LoginProvider>(context);
+    final isLogged = loginProvider.isLoggedIn;
+
     return Scaffold(
-      body: _mainWidgets[_selectIndex],
+      body: _mainWidgets[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Colors.blue,
         selectedItemColor: Colors.black,
         unselectedItemColor: Colors.blue,
-        items: const [
+        items: [
           BottomNavigationBarItem(
-              icon: Icon(Icons.category), label: 'Categorias'),
-          BottomNavigationBarItem(icon: Icon(Icons.pets), label: 'Pokemons'),
+            icon: Icon(Icons.category),
+            label: 'Categorias',
+          ),
           BottomNavigationBarItem(
-              icon: Icon(Icons.favorite), label: 'Favoritos'),
-          BottomNavigationBarItem(icon: Icon(Icons.login), label: 'Login'),
+            icon: Icon(Icons.pets),
+            label: 'Pokemons',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favoritos',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(isLogged ? Icons.logout : Icons.login),
+            label: isLogged ? 'Log Out' : 'Login',
+          ),
         ],
-        currentIndex: _selectIndex,
-        onTap: _onTapItem,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
       ),
     );
   }
